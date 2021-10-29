00ce063673d86f9638ee0fdcf58e2f2bd8865fe9
shootka <pirathaker315@gmail.com>
2021-10-29 17:06:47 +0300

    add branch and schema for login form

---
+++ client/src/components/From/RegEx.js
@@ -0,0 +1,3 @@
+export const NAME_REGEX = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/
+export const IS_REQUIRED = 'This field is required'
+export const GMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
